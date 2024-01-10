6a1208eca8f02c53a47d4568a58d08c6f3728d54
Hzhao7 <154693760+Hzhao7@users.noreply.github.com>
2024-01-10 09:00:42 -0500

    added the character to main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame
+import os
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("Vampire Dyres")
 clock = pygame.time.Clock()
 running = True
 
@@ -12,6 +14,11 @@ background_pos = [0, 0]   # Initial position of the background
 
 # Tile size should match the background image size
 tile_size = (bg_rect.width, bg_rect.height)
+
+protagonist = pygame.image.load(os.path.join('Assets', 'placeholder_character.png')) #loads the protagonist image
+def draw_player(player):
+    screen.blit(protagonist, (player.x,player.y)) #draws the player onto the screen using the rectangle's coordinates
+player = pygame.Rect(640-64, 360-64, 128, 128) #creates a rectangle for the player to check collision
 
 while running:
     screen.fill("blue")
@@ -40,7 +47,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+    draw_player(player)
     pygame.display.flip()
     clock.tick(60)
 
